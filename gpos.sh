207a01f18894c28474b40066e0161fd746cf5e6c
jmtrs <jmtorres15@gmail.com>
2017-02-07 08:09:44 +0000

    jmtrs commit

---
+++ gpos.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+MAINDIRECTORY=$(dirname "$0")   
+function refresh {
+  inotifywait -r \
+    -e modify,move,create,delete \
+    $MAINDIRECTORY
+}
+
+SCRIPT=gpos.sh          
+function gpos {
+  bash $SCRIPT
+}
+
+git add -A
+git commit -m "jmtrs commit"
+git push
+
+
+while refresh; do
+  gpos
+done
